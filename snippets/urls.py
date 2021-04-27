11c4d5521f29bbcef77b734fc7a758c668fdf293
donhwi94 <ehsgnl123@naver.com>
2021-04-27 13:44:19 +0900

    2 - Requests and responses

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 
+from rest_framework.urlpatterns import format_suffix_patterns
+
 from snippets import views
 
 urlpatterns = [
@@ -7,3 +9,4 @@ urlpatterns = [
     path('snippets/<int:pk>/', views.snippet_detail),
 ]
 
+urlpatterns = format_suffix_patterns(urlpatterns)
